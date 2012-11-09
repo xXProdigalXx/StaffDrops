3134110b0174690562b714cb2222fe6824f669d2
Will Zeller <xXProdigalXx@gmail.com>
2012-11-09 11:48:22 -0700

    Bugfix?

--- src/StaffDrops/republica/devteam/Legendarys.java
+++ src/StaffDrops/republica/devteam/Legendarys.java
@@ -24,52 +24,63 @@ public class Legendarys {
 	public Legendarys(CraftItemStack cis){
 		LegendaryItem = cis;
 		
-		display = new NBTTagCompound();
-		
 		tags = LegendaryItem.getHandle().tag;
 		
 		if(tags == null){
 			tags = new NBTTagCompound();
 		}
+		
+		if(tags.hasKey("Display")){
+			display = new NBTTagCompound();
+		}
+		
 
 	}
 	
 	public void setItemName(String _name){
-		name = _name;
-		display.setString("Name", name);
+		if(tags.hasKey("Name")){
+			name = _name;
+			display.setString("Name", name);
+		}
 	}
 	
 	public void setItemRarity(int i){
-		switch(i){
-		case 1:
-			name = name + ChatColor.GREEN;
-			setItemName(name);
-			break;
-		case 2:
-			name = name + ChatColor.BLUE;
-			setItemName(name);
-			break;
-		case 3:
-			name = name + ChatColor.RED;
-			setItemName(name);
-			break;
-		case 4: 
-			name = name + ChatColor.YELLOW;
-			setItemName(name);
-			break;
+		if(tags.hasKey("Name")){
+			switch(i){
+			case 1:
+				name = ChatColor.GREEN + name;
+				setItemName(name);
+				break;
+			case 2:
+				name = ChatColor.BLUE + name;
+				setItemName(name);
+				break;
+			case 3:
+				name = ChatColor.RED + name;
+				setItemName(name);
+				break;
+			case 4: 
+				name =  ChatColor.YELLOW + name;
+				setItemName(name);
+				break;
+			}
 		}
 	}
 	
 	public void setItemLore(String _lore){
-		lore = ChatColor.DARK_PURPLE + _lore;
-		NBTTagString temp = new NBTTagString(lore);
-		
-		display.set("Lore", temp);
+		if(display.hasKey("Lore")){
+			lore = ChatColor.DARK_PURPLE + _lore;
+			NBTTagString temp = new NBTTagString(lore);
+			
+			display.set("Lore", temp);
+		}
 	}
 	
 	public ItemStack getItemStack(){
 		
-		tags.setCompound("display", display);
+		if(display != null){
+			tags.setCompound("display", display);
+		}
 		
 		return LegendaryItem;
 	}
